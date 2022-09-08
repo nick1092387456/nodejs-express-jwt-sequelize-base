3eeb009ec3f47c6248f3b9527641a9494ab596fa
Bumble <nick1092387456@gmail.com>
2022-09-08 18:15:54 +0800

    feat: modify api unauthorize response

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,14 @@
 const passport = require('../config/passport')
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user)
+      return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    if(user)
+      return res.status(200).json({ status: 'success', message: '驗證成功' })
+    next()
+  })(req, res, next)
+}
+
 const authenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
