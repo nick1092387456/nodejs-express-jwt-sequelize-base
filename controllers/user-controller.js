8f85d5cd8e023ddfe906ef5a9780e303d6a48521
Bumble <nick1092387456@gmail.com>
2022-09-08 13:59:04 +0800

    edit: add signUp error handle

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,24 +8,23 @@ const userController = {
   },
   signUp: async (req, res) => {
     try {
+      if (req.body.password !== req.body.passwordCheck)
+        throw '兩次密碼輸入不一致!'
       const isRegistered = await User.findOne({
         where: { email: req.body.email },
       })
-      if (isRegistered) {
-        return res.json({ status: 'error', message: '信箱重複!' })
-      } else {
-        await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: bcrypt.hashSync(
-            req.body.password,
-            bcrypt.genSaltSync(10),
-            null
-          ),
-        })
-      }
+      if (isRegistered) throw '信箱已被註冊!'
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(
+          req.body.password,
+          bcrypt.genSaltSync(10),
+          null
+        ),
+      })
     } catch (err) {
-      res.json({ status: 'error', message: err })
+      return res.json({ status: 'error', message: err })
     }
     return res.json({ status: 'success', message: '成功註冊帳號！' })
   },
