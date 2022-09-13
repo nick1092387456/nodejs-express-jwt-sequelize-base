6ea14caa3da3c31dbce59cfb41c37dccfddcf798
Bumble <nick1092387456@gmail.com>
2022-09-13 11:51:36 +0800

    feat: user-services signUp

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,31 +1,11 @@
-const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
 const jwt = require('jsonwebtoken')
 const userServices = require('../services/user-services')
 
 const userController = {
-  signUp: async (req, res) => {
-    try {
-      if (req.body.password !== req.body.passwordCheck)
-        throw '兩次密碼輸入不一致!'
-      const isRegistered = await User.findOne({
-        where: { email: req.body.email },
-      })
-      if (isRegistered) throw '信箱已被註冊!'
-      await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(
-          req.body.password,
-          bcrypt.genSaltSync(10),
-          null
-        ),
-      })
-    } catch (err) {
-      return res.json({ status: 'error', message: err })
-    }
-    return res.json({ status: 'success', message: '成功註冊帳號！' })
+  signUp: (req, res, next) => {
+    userServices.signUp(req, (err, data) =>
+      err ? next(err) : res.status(200).json(data)
+    )
   },
   signIn: (req, res, next) => {
     try {
@@ -38,44 +18,6 @@ const userController = {
     } catch (err) {
       next(err)
     }
-    // original................
-    // 檢查必要資料
-    // if (!req.body.email || !req.body.password) {
-    //   return res.json({
-    //     status: 'error',
-    //     message: "required fields didn't exist",
-    //   })
-    // }
-    // // 檢查 user 是否存在與密碼是否正確
-    // let username = req.body.email
-    // let password = req.body.password
-
-    // User.findOne({ where: { email: username } }).then((user) => {
-    //   if (!user)
-    //     return res
-    //       .status(401)
-    //       .json({ status: 'error', message: 'no such user found' })
-    //   if (!bcrypt.compareSync(password, user.password)) {
-    //     return res
-    //       .status(401)
-    //       .json({ status: 'error', message: 'passwords did not match' })
-    //   }
-
-    //   // 簽發 token
-    //   var payload = { id: user.id }
-    //   var token = jwt.sign(payload, process.env.JWT_SECRET)
-    //   return res.json({
-    //     status: 'success',
-    //     message: 'ok',
-    //     token: token,
-    //     user: {
-    //       id: user.id,
-    //       name: user.name,
-    //       email: user.email,
-    //       isAdmin: user.isAdmin,
-    //     },
-    //   })
-    // })
   },
   getUsers: (req, res, next) => {
     userServices.getUsers(req, (err, data) =>
--- services/user-services.js
+++ services/user-services.js
@@ -1,4 +1,31 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
 const userServices = {
+  signUp: async (req, callback) => {
+    try {
+      if (req.body.password !== req.body.passwordCheck)
+        throw '兩次密碼輸入不一致!'
+      const isRegistered = await User.findOne({
+        where: { email: req.body.email },
+      })
+      if (isRegistered) throw '信箱已被註冊!'
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(
+          req.body.password,
+          bcrypt.genSaltSync(10),
+          null
+        ),
+      })
+      return callback(null, { status: 'success', message: '成功註冊帳號！' })
+    } catch (err) {
+      return callback(null, { status: 'error', message: err })
+    }
+  },
+
   getUsers: (req, callback) => {
     try {
       return callback(null, {
